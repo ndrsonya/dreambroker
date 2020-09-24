0513d857056bee4bb4f51df2d845cecb25656928
ndrsonya <ViktorAnderson@MBP-Viktor.home>
2020-09-24 17:09:34 +0300

    somr things fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.scss';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
